99c909e6e1c78a9081b50dd75f6b2381448ab998
JavokhirJambulov <javokhir@example.com>
2023-05-29 01:08:27 +0000

    Commiting the new delete function

--- add100RandomBooks.py
+++ add100RandomBooks.py
@@ -34,12 +34,26 @@ def addBook(book, apiKey):
     else:
         raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to add book {book}.")
 
+#delete function
+def deleteBook(id, apiKey):
+    r = requests.delete(
+        f"{APIHOST}/api/v1/books/{id}", 
+        headers = {
+            "Content-type": "application/json",
+            "X-API-Key": apiKey
+            }
+    )
+    if r.status_code == 200:
+        print(f"Book with {id} is deleted.")
+    else:
+        raise Exception(f"Error code {r.status_code} and text {r.text}, while trying to add book {book}.")
+
 # Get the Auth Token Key
 apiKey = getAuthToken()
 
 # Using the faker module, generate random "fake" books
 fake = Faker()
-# the next for loop will add new 25 books
+# # the next for loop will add new 25 books
 for i in range(107, 132): 
     fakeTitle = fake.catch_phrase()
     fakeAuthor = fake.name()
@@ -47,3 +61,12 @@ for i in range(107, 132):
     book = {"id":i, "title": fakeTitle, "author": fakeAuthor, "isbn": fakeISBN}
     # add the new random "fake" book using the API
     addBook(book, apiKey) 
+
+for i in range(1, 6): 
+    # will delete first 5 books
+    deleteBook(i, apiKey) 
+    
+for i in range(128, 133): 
+    # will delete last 5 books
+    deleteBook(i, apiKey) 
+
